ed5247f8d0853ae63512bf763693049d619cc2b6
tonywang <wyhuster@gmail.com>
2014-01-07 15:58:51 +0800

    remove useless code

--- web/index.php
+++ web/index.php
@@ -1,30 +1,15 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
+
+<html><head>
+<title>客户端后端稳定性测试</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv=html content=no-cache>
-	<title>客户端后端稳定性测试</title>
-	<link rel="stylesheet" type="text/css" href="js/extjs/resources/css/ext-all.css" />
-	<script type="text/javascript" src="js/extjs/ext-all-debug.js"></script>
-	<script type="text/javascript" src="js/settings.js"></script>
-	<script type="text/javascript" src="js/index.js"></script>
-	<script type="text/javascript" language="javascript">
-Ext.require(['*']);
-Ext.onReady(function() {
-     
-    Ext.create('Ext.container.Viewport',{
-		renderTo:Ext.getBody(),
-        layout: {
-            type: 'border',
-            padding: 5
-        },
-        defaults: {
-            split: true
-        },
-items: [toppanel,menu,main,statuspanel]
-    });
-});
-	</script>
+<link href="./img/style.css" type="text/css" rel="stylesheet">
 </head>
-<body></body>
+<frameset border=0 frameSpacing=0 rows=80,* frameBorder=no cols=*>
+	<frame name=topFrame src="top.php" noResize scrolling=no>
+	<frameset border=1 frameSpacing=0 frameBorder=yes cols=200,*>
+		<frame id=f1 name=f1 marginWidth=5 marginHeight=50 src="menu.php" noResize>
+		<!--frame id=f2 name=window_main src="wmain.html"-->
+		<frame id=f2 name=window_main src="intro.php">
+	</frameset>
+</frameset>
 </html>
--- web/st_url/urls.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$url = $_GET['url'];
-	$params = $_GET['params'];
-	$array = array("http://www.baidu.com/?a=1", "http://www.baidu.com/?b=1", "http://www.baidu.com/?a=1&b=1");    
-	echo json_encode($array)
-	
-?>
